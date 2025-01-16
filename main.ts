931d602b0e555b6899bd57d2d75951fd7cb9babf
toonvanbeneden <toonvanbeneden@gmail.com>
2025-01-16 10:11:58 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,56 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -400) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) > 400) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) > 550) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) < -550) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) < 40 && input.acceleration(Dimension.Y) > -40) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) > -40 && input.acceleration(Dimension.X) < 40) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
